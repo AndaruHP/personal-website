c319b91ac177162021b39caeb38251b77033a256
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2024-11-24 22:26:13 +0700

    show database on homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('homepage');
+Route::get('/', [PostController::class, 'index']);
+
+Route::get('/posts', function () {
+    return redirect('/');
 });
+Route::get('/posts/{slug}', [PostController::class, 'show']);
